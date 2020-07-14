56f151e17d936c09a09d01d93c5b1628eab427c5
Volker Buzek <volker.buzek@js-soft.com>
2020-07-14 21:15:00 +0200

    wip: sap.ui.define dependencies

--- packages/ui5-app/webapp/test/unit-new/Basics.test.js
+++ packages/ui5-app/webapp/test/unit-new/Basics.test.js
@@ -1,6 +1,15 @@
+const moduleLoader = require("ui5-simple-require")
 const ui5require = require("ui5-simple-require").ui5require
 
-test("plain module inits correctly", () => {
+test("plain ui5 module inits correctly", () => {
     const PlainModule = ui5require("../../lib/baseModule.js")
     expect(PlainModule.someFunc()).toStrictEqual("someFunc")
 })
+
+// providing sap.ui.core works in principle via
+// const coreLib = moduleLoader.importLib("./path/to/sap.ui.core/files", "sap/ui/core", "sap.ui.core")
+// but fails at the depencendies of the lib itself
+
+// test("base controller has working lifecycle", () => {
+//     const BaseController = ui5require("../../controller/BaseController.js")
+// })
