725c958de3ebdd3de0512559abdce2983d5c8b7a
Volker Buzek <volker.buzek@js-soft.com>
2020-10-20 14:07:55 +0200

    test(wdi5): showcase wdio plugin

--- packages/ui5-app/webapp/test/wdi5/basic.test.js
+++ packages/ui5-app/webapp/test/wdi5/basic.test.js
@@ -1,12 +1,29 @@
 describe("basics", ()=> {
-    it("should click the 'boo'-button", () => {
+    it("should navigate successfully to list page", () => {
         const _button = {
             selector: {
-                id: "booButton",
-                controlType: "sap.m.Button",
+                id: "navButton",
                 viewName: "test.Sample.view.Main"
             }
         }
         browser.asControl(_button).firePress()
+
+        const _list = {
+            selector: {
+                id: "PeopleList",
+                viewName: "test.Sample.view.Other"
+            }
+        }
+        expect(browser.asControl(_list).getVisible()).toBeTruthy()
+    })
+    it("should check that list is bound", () => {
+        const _list = {
+            selector: {
+                id: "PeopleList",
+                viewName: "test.Sample.view.Other"
+            }
+        }
+        const listItems = browser.asControl(_list).getItems()
+        expect(listItems.length).toBeGreaterThanOrEqual(1)
     })
 })
