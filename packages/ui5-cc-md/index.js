79a14e8236996f839aa250e4af165bcdfb817252
Volker Buzek <volker.buzek@js-soft.com>
2021-02-04 17:02:03 +0100

    chore(ui5-cc-md): inital commit

---
+++ packages/ui5-cc-md/index.js
@@ -0,0 +1,7 @@
+sap.ui.define(["sap/ui/core/Control"], (Control) => {
+    return Control.extend("ui5.cc.md", {
+        metadata: {},
+        init() {},
+        renderer(oRM, oControl) {}
+    })
+})
