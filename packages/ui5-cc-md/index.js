935f61b45ea21df32434ec0ddefa5c875461ed61
Volker Buzek <volker.buzek@js-soft.com>
2021-02-04 22:04:34 +0100

    feat(mvp): md -> html

--- packages/ui5-cc-md/index.js
+++
@@ -1,7 +0,0 @@
-sap.ui.define(["sap/ui/core/Control"], (Control) => {
-    return Control.extend("ui5.cc.md", {
-        metadata: {},
-        init() {},
-        renderer(oRM, oControl) {}
-    })
-})
